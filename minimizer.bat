f6fbd609f21d43210ceb1820e497ee49b07852d3
David Esperalta <davidesperalta@gmail.com>
2026-03-15 02:01:18 +0100

    Minor refactorings
    Minor refactorings

--- minimizer.bat
+++ minimizer.bat
@@ -5,7 +5,7 @@ SET COMP=.\shared\minimizer\NUglifyApp.exe
 
 REM Smeacher Admin
 
-%COMP% "./www/decsoft-cameras.js" -o "./www/decsoft-cameras.min.js" -enc:in "utf-8" -clobber:true
+%COMP% "./www/decsoft-cameras/decsoft-cameras.js" -o "./www/decsoft-cameras/decsoft-cameras.min.js" -enc:in "utf-8" -clobber:true
 
 echo Finished!
 
