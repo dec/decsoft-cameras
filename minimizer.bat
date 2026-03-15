9083bf84b51652a30dbf734ba41a4cd648fed251
David Esperalta <davidesperalta@gmail.com>
2026-03-15 20:51:28 +0100

    Minor changes
    Minor changes

--- minimizer.bat
+++ minimizer.bat
@@ -3,8 +3,6 @@ cls
 
 SET COMP=.\shared\minimizer\NUglifyApp.exe
 
-REM Smeacher Admin
-
 %COMP% "./www/decsoft-cameras/decsoft-cameras.js" -o "./www/decsoft-cameras/decsoft-cameras.min.js" -enc:in "utf-8" -clobber:true
 
 echo Finished!
